c816c3bc635b66e1e2c2f5ef255f2cb955a75d83
theodore brockman <tmanbrock@gmail.com>
2024-07-23 09:35:59 -0400

    Re-named Safari upload.

---
+++ generated/safari-xcode/Browser Ext for OpenTelemetry/Browser Ext for OpenTelemetry/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Browser Ext for OpenTelemetry
+//
+//  Created by theodore brockman on 7/22/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
